25a1127193cedac77ca380ac5c66afb61b93ccb1
abdallah nasser <bedo2boy@gmail.com>
2023-02-02 08:46:26 +0200

    inital commit and fonts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./assets/Logo.svg";
+import image from "./assets/restauranfood.jpg"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid">
+      <nav>
+        <ul className="nav">
+          <li>
+            <img src={logo} alt="logo" />
+          </li>
+          <li>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Menu</li>
+              <li>Reservations</li>
+              <li>Order Online</li>
+              <li>Login</li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <section className="hero">
+      <div>
+      <div>
+        <h1>Little Lemon</h1>
+        <h3>Chicago</h3>
+        <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+      </div>
+        <button>Reserve a table</button>
+      </div>
+        <img className="hero-image" src={image} alt="logo" />
+      </section>
+      <section className="highlights">
+        <h1>This weeks specials!</h1>
+        <button>Online Menu</button>
+        <card></card>
+        <card></card>
+        <card></card>
+      </section>
     </div>
   );
 }
